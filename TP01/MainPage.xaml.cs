b5cc3947a78898d3da1bc0949607dd7606b2eae7
Pedro <pedrohenriqueperpetuo@gmail.com>
2024-10-07 16:57:24 -0300

    TP01

---
+++ TP01/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace TP01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOkClicked(object sender, EventArgs e)
+        {
+            if (idEntry.Text == "admin" && passwordEntry.Text == "senha@dmin")
+            {
+                DisplayAlert("OK", "logou!", "OK");
+            }
+            else
+            {
+                DisplayAlert("Erro", "credenciais erradas...", "OK");
+            }
+        }
+
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+            idEntry.Text = "";
+            passwordEntry.Text = "";
+            idEntry.Focus();
+        }
+
+        private void OnCreditsClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Créditos", "Pedro Henrique Perpétuo CB3021688", "OK");
+        }
+    }
+}
+// Pedro Henrique Perpétuo CB3021688
